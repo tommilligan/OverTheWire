3506e69a7a17f3fda00958d7e39288d47998e481
Tom Milligan <tom@tommilligan.net>
2017-06-12 23:30:04 +0100

    Made filepath absolute

--- bandit/bandit7.sh
+++ bandit/bandit7.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Grep data file for the given keyword
-grep "millionth" data.txt | \
+grep "millionth" ~/data.txt | \
 
 # Throw away the first column and return the second
 awk '{ print $2 }'
