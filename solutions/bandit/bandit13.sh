b0f347f2faaca90a44147ddfc560efb4a39c4e72
Tom Milligan <tom@tommilligan.net>
2017-06-15 00:03:01 +0100

    hk

--- solutions/bandit/bandit13.sh
+++ solutions/bandit/bandit13.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Move to a temporary directory
+cd $(mktemp -d)
+
 # Secure copy password file using bandit14's private key
-# From the known location to our current home directory
+# From the known location to our current directory
+scp -i ~/sshkey.private -o StrictHostKeyChecking=no -P 2220 bandit14@localhost:/etc/bandit_pass/bandit14 .
+
 # Once scp is completed read the resulting file
-scp -i sshkey.private -P 2220 bandit14@localhost:/etc/bandit_pass/bandit14 ~ && cat ~/bandit14
+cat bandit14
