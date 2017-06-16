978d8b8908b6c95ce6779dbe929980855dcaa185
Tom Milligan <tom@tommilligan.net>
2017-06-16 23:37:58 +0100

    Updated tests of OTWException

--- tests/test__init__.py
+++ tests/test__init__.py
@@ -4,25 +4,46 @@ import unittest
 
 import OverTheWire
 
-class TestOTWLevelKnown(unittest.TestCase):
+class TestOTWLevelSetup(unittest.TestCase):
+    def test_otw_level_setup_known_int(self):
+        level = OverTheWire.OTWLevel("bandit", 0)
+        self.assertEqual(level.levelNumber, 0)
+        self.assertEqual(level.levelName, "bandit0")
+        for k in ["domain", "port"]:
+            self.assertTrue(k in level.config)
+
+    def test_otw_level_setup_known_str(self):
+        level = OverTheWire.OTWLevel("bandit", "0")
+        self.assertEqual(level.levelNumber, 0)
+        self.assertEqual(level.levelName, "bandit0")
+        for k in ["domain", "port"]:
+            self.assertTrue(k in level.config)
+
+    def test_otw_level_setup_unknown(self):
+        with self.assertRaises(OverTheWire.OTWException):
+            level = OverTheWire.OTWLevel("spam", "0")
+
+class TestOTWLevelNormal(unittest.TestCase):
     def setUp(self):
         self.level = OverTheWire.OTWLevel("bandit", 0)
+        self.level.savePassword("eggs")
 
-    def test_otw_level_attrbutes(self):
-        self.assertEqual(self.level.levelNumber, 0)
-        self.assertEqual(self.level.levelName, "bandit0")
-        for k in ["domain", "port"]:
-            self.assertTrue(k in self.level.config)
+    def test_otw_level_savePassword(self):
+        self.level.savePassword("spam")
 
-    def test_otw_level_sync_password(self):
-        self.level.savePassword('spam')
+    def test_otw_level_loadPassword(self):
         password = self.level.loadPassword()
-        self.assertEqual(password, 'spam')
+        self.assertEqual(password, "eggs")
 
     def test_otw_level_connectionCommand(self):
-        self.level.savePassword('spam')
         command = self.level.connectionCommand()
-        self.assertEqual(command, 'sshpass -p spam ssh -o StrictHostKeyChecking=no -p 2220 bandit0@bandit.labs.overthewire.org')
+        self.assertEqual(command, "sshpass -p eggs ssh -o StrictHostKeyChecking=no -p 2220 bandit0@bandit.labs.overthewire.org")
 
     def tearDown(self):
         self.level.savePassword("bandit0")
+
+class TestOTWLevelEdge(unittest.TestCase):
+    def test_otw_level_loadPassword_not_exist(self):
+        level = OverTheWire.OTWLevel("bandit", -1)
+        with self.assertRaises(OverTheWire.OTWException):
+            password = level.loadPassword()
