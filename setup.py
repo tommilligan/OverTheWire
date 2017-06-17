36cfbbcf75b8929f5f94f0f8e7b4bdf3119080f9
Tom Milligan <tom@tommilligan.net>
2017-06-17 01:19:42 +0100

    Version bump to v0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OverTheWire',
-    version='0.0.2',
+    version='0.0.3',
     license='Apache License 2.0',
     url='https://github.com/tommilligan/OverTheWire/',
     author='Tom Milligan',
